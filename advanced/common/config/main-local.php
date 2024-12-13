f0031c790c1672aafdef27c1b29c30832fe066c2
ya-yaa <162297857+ya-yaa@users.noreply.github.com>
2024-12-13 18:32:54 +0800

    需求+设计+个人作业

--- advanced/common/config/main-local.php
+++ advanced/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=webdb',
-            'username' => 'root',
-            'password' => '040702',
+            'dsn' => 'mysql:host=localhost;dbname=homework',
+            'username' => 'homework',
+            'password' => '123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
